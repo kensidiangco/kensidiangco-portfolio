b73a625b13dad0d3c24d0492107380806751037d
kensidiangco <kensidiangco@gmail.com>
2020-11-21 20:42:58 +0800

    build

---
+++ my-portfolio/build/precache-manifest.6d949b4ee5ee798c647c7e62a7458dca.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afb3ac06d22271d5b261cee37977dcad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "57b64b06c40a75211561",
+    "url": "/static/css/main.3b366e36.chunk.css"
+  },
+  {
+    "revision": "04c2d4c6a1fb1e1c8210",
+    "url": "/static/js/2.eb002387.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.eb002387.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57b64b06c40a75211561",
+    "url": "/static/js/main.b185c784.chunk.js"
+  },
+  {
+    "revision": "efe25b4b8d1f7af9da4b",
+    "url": "/static/js/runtime-main.6b3488d1.js"
+  },
+  {
+    "revision": "86105d941c83bbe7587a08dfe1c410c2",
+    "url": "/static/media/AddTenant.86105d94.png"
+  },
+  {
+    "revision": "c5d58a5035613189a07ed6aaa9dc3b54",
+    "url": "/static/media/Dashboard.c5d58a50.png"
+  },
+  {
+    "revision": "fae4e87ff46c24d8f1df4e872098568d",
+    "url": "/static/media/Dashboard.fae4e87f.png"
+  },
+  {
+    "revision": "28a43527422d1fd34f0f89e3d45f8465",
+    "url": "/static/media/Details.28a43527.png"
+  },
+  {
+    "revision": "faaa480b6a3903edbbb75ffc318e9fc2",
+    "url": "/static/media/FetchedCountry.faaa480b.png"
+  },
+  {
+    "revision": "a70a2e0b5e703b81a9dc275afed647be",
+    "url": "/static/media/Home.a70a2e0b.png"
+  },
+  {
+    "revision": "86f24bd1d95a7368ec28dfe9ce22a43a",
+    "url": "/static/media/Homepage.86f24bd1.png"
+  },
+  {
+    "revision": "a20e7bcfe1e676e0ec8697445e78519d",
+    "url": "/static/media/Login.a20e7bcf.png"
+  },
+  {
+    "revision": "b6f5ff6dcf0b1010129c5ec61e27f0a1",
+    "url": "/static/media/Login.b6f5ff6d.png"
+  },
+  {
+    "revision": "12e315ee3f8014ead278b4c9328d83a2",
+    "url": "/static/media/Logout.12e315ee.png"
+  },
+  {
+    "revision": "25fa70e6964c763ced31235788377ac5",
+    "url": "/static/media/Pay.25fa70e6.png"
+  },
+  {
+    "revision": "dc3a6f5cf35213d477243ec1e687a589",
+    "url": "/static/media/Register.dc3a6f5c.png"
+  },
+  {
+    "revision": "067db242dcbfba83b6bc73c0fe0f6218",
+    "url": "/static/media/TenantStatus.067db242.png"
+  },
+  {
+    "revision": "40ac2c326d5d1e6adae684e8031dab7b",
+    "url": "/static/media/Update.40ac2c32.png"
+  },
+  {
+    "revision": "9196f70ac226f9759b77e0c7b5f38653",
+    "url": "/static/media/app__background.9196f70a.png"
+  },
+  {
+    "revision": "dc018e0e6700de99b923c323065bbbcd",
+    "url": "/static/media/app__background2.dc018e0e.png"
+  },
+  {
+    "revision": "b39738195df752639b16d3d0b7c5b90a",
+    "url": "/static/media/app__header.b3973819.png"
+  }
+]);
